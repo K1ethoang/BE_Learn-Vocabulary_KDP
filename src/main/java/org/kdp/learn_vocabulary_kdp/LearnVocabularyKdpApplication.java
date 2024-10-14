29e392724e9490aba8d204ec7efede193279e20f
Kiet Hoang Gia <kiethoang101@gmail.com>
2024-10-14 17:10:39 +0700

    init project

---
+++ src/main/java/org/kdp/learn_vocabulary_kdp/LearnVocabularyKdpApplication.java
@@ -0,0 +1,13 @@
+package org.kdp.learn_vocabulary_kdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnVocabularyKdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnVocabularyKdpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kdp/learn_vocabulary_kdp/LearnVocabularyKdpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kdp.learn_vocabulary_kdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnVocabularyKdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
