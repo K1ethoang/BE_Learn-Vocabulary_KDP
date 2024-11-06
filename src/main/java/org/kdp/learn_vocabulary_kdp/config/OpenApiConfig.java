fbc6c96e059b6e17d8da902740ddb609e19f7c7b
Kiet Hoang Gia <88199151+K1ethoang@users.noreply.github.com>
2024-11-06 23:14:41 +0700

    update: OpenAPI configuration and add API docs path (#3) (#4)

--- src/main/java/org/kdp/learn_vocabulary_kdp/config/OpenApiConfig.java
+++ src/main/java/org/kdp/learn_vocabulary_kdp/config/OpenApiConfig.java
@@ -1,35 +1,29 @@
 package org.kdp.learn_vocabulary_kdp.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
-@ComponentScan(basePackages = {"org.kdp.learn_vocabulary_kdp.controller"})
+@ComponentScan(basePackages = "org.kdp.learn_vocabulary_kdp.controller.v1")
 public class OpenApiConfig {
     @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-            .info(new Info()
-            .title("API Documentation")
-            .version("1.0")
-            .description("API documentation for website learn English vocabulary")
-            .contact(new Contact()
-                .name("Kiet Hoang")
-                .email("kiethoang101.dev@gmail.com")
-                .url("https://github.com/K1ethoang")));
-    }
+    public OpenAPI defineOpenApi() {
+        Server server = new Server();
+        server.setDescription("Development");
 
-    @Bean
-    public Components customComponents() {
-        return new Components()
-                .addSecuritySchemes("basicScheme", new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("basic"));
+        Contact myContact = new Contact();
+        myContact.setName("Kiet Hoang");
+        myContact.setEmail("kiethoang101.dev@gmail.com");
+        myContact.setUrl("https://github.com/K1ethoang");
+
+        Info information = new Info().title("Learn Vocabulary KDP System API").version("1.0").description("API documentation for website learn English vocabulary").contact(myContact);
+        return new OpenAPI().info(information).servers(List.of(server));
     }
 }
