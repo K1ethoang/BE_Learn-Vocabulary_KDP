6ef8ec57df119cc586935f04e5dc44cc8dc346f1
Kiet Hoang Gia <kiethoang101@gmail.com>
2024-12-18 18:26:59 +0700

    feat: create docker file, docker compose

---
+++ init.sql
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2024. K1ethoang
+ * @Author: Kiet Hoang Gia
+ * @LastModified: 2024/12/18 - 17:07 PM (ICT)
+ */
+
+-- Tạo schema (database)
+CREATE DATABASE IF NOT EXISTS Vocabulary_KDP;
+
+-- Sử dụng schema
+USE Vocabulary_KDP;
