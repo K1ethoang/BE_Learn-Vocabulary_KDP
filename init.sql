f8cff7f4a9776743372296b51cd998ba52832641
Kiet Hoang Gia <kiethoang101@gmail.com>
2024-12-19 00:33:22 +0700

    update: configuration for docker

--- init.sql
+++ init.sql
@@ -1,11 +1,8 @@
 /*
  * Copyright (c) 2024. K1ethoang
  * @Author: Kiet Hoang Gia
- * @LastModified: 2024/12/18 - 17:07 PM (ICT)
+ * @LastModified: 2024/12/19 - 00:27 AM (ICT)
  */
 
 -- Tạo schema (database)
 CREATE DATABASE IF NOT EXISTS Vocabulary_KDP;
-
--- Sử dụng schema
-USE Vocabulary_KDP;
