eef12bde0330cdae91a9ebaf92dee0f6eedf0d2c
Kiet Hoang Gia <kiethoang101@gmail.com>
2024-12-20 11:17:05 +0700

    fix: cors

--- src/main/java/org/kdp/learn_vocabulary_kdp/configuration/SecurityConfig.java
+++ src/main/java/org/kdp/learn_vocabulary_kdp/configuration/SecurityConfig.java
@@ -1,7 +1,7 @@
 /*************************************************
  * Copyright (c) 2024. K1ethoang
  * @Author: Kiet Hoang Gia
- * @LastModified: 2024/12/20 - 10:48 AM (ICT)
+ * @LastModified: 2024/12/20 - 11:16 AM (ICT)
  ************************************************/
 package org.kdp.learn_vocabulary_kdp.configuration;
 
@@ -13,8 +13,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
@@ -40,10 +38,6 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-
-        http.sessionManagement(sessionManagementConfigurer ->
-                sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
                 .requestMatchers(PUBLIC_ENDPOINTS)
                 .permitAll()
