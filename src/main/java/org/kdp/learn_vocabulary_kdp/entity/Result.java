0f0fea5047bc2217f2ad5d0eb88e0481e7ab9c06
Kiet Hoang Gia <kiethoang101@gmail.com>
2024-12-26 13:23:29 +0700

    feat: add entity

--- src/main/java/org/kdp/learn_vocabulary_kdp/entity/Result.java
+++ src/main/java/org/kdp/learn_vocabulary_kdp/entity/Result.java
@@ -1,46 +1,45 @@
 /*************************************************
  * Copyright (c) 2024. K1ethoang
  * @Author: Kiet Hoang Gia
- * @LastModified: 2024/12/26 - 13:04 PM (ICT)
+ * @LastModified: 2024/12/26 - 13:02 PM (ICT)
  ************************************************/
+
 package org.kdp.learn_vocabulary_kdp.entity;
 
+
 import jakarta.persistence.*;
-import lombok.*;
-import lombok.experimental.FieldDefaults;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.util.Date;
-import java.util.List;
 
-@Entity(name = "exams")
+@Entity(name = "results")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
-public class Exam {
+public class Result {
     @Id
     @UuidGenerator
-    @Column(name = "exam_id")
+    @Column(name = "result_id")
     String id;
-
-    @Column(name = "total_questions")
-    int totalQuestions;
 
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created_at", nullable = false, updatable = false)
     Date createdAt;
 
-    @OneToMany(mappedBy = "exam")
-    List<Question> questions;
+    @Column(name = "correct_count",nullable = false)
+    int correctCount;
 
-    @OneToOne(mappedBy = "exam")
-    Result result;
+    @Column(name = "details", columnDefinition = "JSON")
+    String details;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "topic_id")
-    Topic topic;
+    @OneToOne
+    @JoinColumn(nullable = false, name = "exam_id")
+    Exam exam;
 }
